1bc64b799dd9302b67f709dc7bb81b23fdae445d
rishikar <rishikar9849@gmail.com>
2023-05-03 21:43:38 +0530

    Transformer based model without cross attention

---
+++ cache.py
@@ -0,0 +1,10 @@
+import gc
+import torch
+def report_gpu():
+   print(torch.cuda.list_gpu_processes())
+   gc.collect()
+   torch.cuda.empty_cache()
+
+import gc
+gc.collect()
+torch.cuda.empty_cache()
